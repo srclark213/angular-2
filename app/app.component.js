88b213d6fa062435b1b85ad4741d7688cc7c75e9
srclark <stephen.r.clark@hsutx.edu>
2016-11-06 17:06:02 -0600

    built the first simple product-list component

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'pm-app',
-            template: "\n    <div class='container'>\n        <h1>{{pageTitle}}</h1>\n        <div>My First Component</div>\n    </div>\n    "
+            template: "\n    <div class='container'>\n        <h1>{{pageTitle}}</h1>\n        <pm-products></pm-products>\n    </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
